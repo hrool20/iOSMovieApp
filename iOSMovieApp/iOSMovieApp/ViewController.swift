5ba6cbec24fe2201a2c001682fd09f77240c4c67
Hugo Andres Rosado <u201411897@upc.edu.pe>
2020-09-09 16:40:33 -0500

    iOS Project added.

---
+++ iOSMovieApp/iOSMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSMovieApp
+//
+//  Created by Hugo Andres Rosado on 9/9/20.
+//  Copyright © 2020 Hrool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSMovieApp/iOSMovieAppTests/iOSMovieAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOSMovieAppTests.swift
+//  iOSMovieAppTests
+//
+//  Created by Hugo Andres Rosado on 9/9/20.
+//  Copyright © 2020 Hrool. All rights reserved.
+//
+
+import XCTest
+@testable import iOSMovieApp
+
+class iOSMovieAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
